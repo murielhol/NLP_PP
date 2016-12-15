566fb73b29303470eaf25cbc56f9c76d29e8815a
victormilewski1994 <victormilewski@gmail.com>
2016-12-15 15:58:00 +0100

    fixed merging
    arks and my version of graph construction are now merged and the correct
    working version is ready to use

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ from graph_construction import Graphs
 import numpy as np 
 
 graph = Graphs('data/en_50000','data/pt_50000','data/en-pt_50000.forward.align')
+words = graph.get_english_wordlist()
+for w in words:
+    print w+'\n'
