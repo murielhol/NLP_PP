f2a9a7e988ed3bbf48868bbc499fabcef44ba831
victormilewski1994 <victormilewski@gmail.com>
2016-12-15 16:01:20 +0100

    fixed files and test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from graph_construction import Graphs
 import numpy as np 
 
-graph = Graphs('data/en_50000','data/pt_50000','data/en-pt_50000.forward.align')
+graph = Graphs('data/en_50000_noempty','data/pt_50000_noempty','data/en-pt_50000.forward.align')
 words = graph.get_english_wordlist()
 for w in words:
     print w+'\n'
