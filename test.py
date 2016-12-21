4a1bf3f9a30f4ea2517e0ef4aa16316da9bf65e7
Muriël Hol <murielhol@hotmail.com>
2016-12-21 14:00:59 +0100

    English dist.matrix
    Matrix with the distribution vector for each english word

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-from graph_construction import Graphs
+
+from graph_construction import *
 import numpy as np 
 
+
 graph = Graphs('data/en_50000_noempty','data/pt_50000_noempty','data/en-pt_50000.forward.align')
-words = graph.get_english_wordlist()
-for w in words:
-    print w+'\n'
+
+
+
+# words = graph.get_english_wordlist()
+# for w in words:
+#     print w+'\n'
